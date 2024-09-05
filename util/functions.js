262f9665233abc237094ef277fa739dd4e63eb19
M336 <75208577+M336G@users.noreply.github.com>
2024-09-05 15:01:26 +0200

    Prepare functions for user & password update
    Also add notes to all functions

--- util/functions.js
+++ util/functions.js
@@ -1,25 +1,36 @@
 const fs = require("fs").promises;
+const bcrypt = require("bcrypt");
 
+// Simplied async functions that are related to console logging.
 const log = {
+    // Log an error in the console.
     error: async (message, error) => {
         if (!error) return console.error("[ERROR]", message);
         else return console.error("[ERROR]", message, error);
     },
+
+    // Log a warn in the console.
     warn: async (message, warn) => {
         if (!warn) return console.warn("[WARN]", message);
         else return console.warn("[WARN]", message, warn);
     },
+
+    // Log an info in the console.
     info: async (message, info) => {
         if (!info) return console.info("[INFO]", message);
         else return console.info("[INFO]", message, info);
     },
+
+    // Log a debug in the console.
     debug: async (message, debug) => {
         if (!debug) return console.debug("[DEBUG]", message);
         else return console.debug("[DEBUG]", message, debug);
     }
 };
 
+// Simplied async functions that are related to base64.
 const encode = {
+    // Encode an URL using the base64 format.
     base64encode: async (string) => {
         if (!string) return;
         try {
@@ -29,6 +40,8 @@ const encode = {
             return;
         }
     },
+
+    // Decode an URL using the base64 format.
     base64decode: async (string) => {
         if (!string) return;
         try {
@@ -38,6 +51,8 @@ const encode = {
             return;
         }
     },
+
+    // Encode an URL using the base64 url format.
     base64urlencode: async (string) => {
         if (!string) return;
         try {
@@ -47,6 +62,8 @@ const encode = {
             return;
         }
     },
+
+    // Decode an URL using the base64 url format.
     base64urldecode: async (string) => {
         if (!string) return;
         try {
@@ -58,6 +75,73 @@ const encode = {
     }
 }
 
+// Simplied async functions that, for most, use the bcrypt module.
+const encrypt = {
+    // Generate a salt and returns it. Returns false if it fails to or one of the values is invalid.
+    generateSalt: async (saltRounds) => {
+        if (!saltRounds) return false;
+        try {
+            const salt = await new Promise((resolve, reject) => {
+                bcrypt.genSalt(saltRounds, (error, salt) => {
+                    if (error) {
+                        log.error("Failed generating password salt:", error);
+                        reject(error);
+                    } else {
+                        resolve(salt);
+                    }
+                });
+            });
+            return salt;
+        } catch (error) {
+            log.error("Failed generating password salt:", error);
+            return false;
+        }
+    },
+
+    // Generate an hash and returns it, using a password and a salt. Returns false if it fails to, or one of the values are invalid.
+    generateHash: async (password, salt) => {
+        if (!password || !salt) return false;
+        try {
+            const hash = await new Promise((resolve, reject) => {
+                bcrypt.hash(password, salt, (error, hash) => {
+                    if (error) {
+                        log.error("Failed generating password hash:", error);
+                        reject(error);
+                    } else {
+                        resolve(hash);
+                    }
+                });
+            });
+            return hash;
+        } catch (error) {
+            log.error("Failed generating password hash:", error);
+            return false;
+        }
+    },
+
+    // Verify the authenticity of a password compared to an hash. Returns false if it fails to, if one of the values is invalid or if the password is invalid.
+    verifyPassword: async (password, hash) => {
+        if (!password || !hash) return false;
+        try {
+            const response = await new Promise((resolve, reject) => {
+                bcrypt.compare(password, hash, (error, response) => {
+                    if (error) {
+                        log.error("Failed checking for password:", error);
+                        reject(error);
+                    } else {
+                        resolve(response);
+                    }
+                });
+            });
+            return response;
+        } catch (error) {
+            log.error("Failed checking for password:", error);
+            return false;
+        }
+    }
+}
+
+// Tries deleting a file. If it fails to, returns false.
 async function deleteFile(filePath) {
     if (!filePath) return false;
     try {
@@ -65,6 +149,7 @@ async function deleteFile(filePath) {
         await fs.unlink(filePath);
         return true;
     } catch (error) {
+        log.error("Failed to delete a file:", error);
         return false;
     }
 }
@@ -72,5 +157,6 @@ async function deleteFile(filePath) {
 module.exports = {
     log,
     encode,
+    encrypt,
     deleteFile
 };
